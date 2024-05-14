90de423ef0edf181dcaa24358be64a45cbf6de27
Gustavo Carbonera <guga_carbo@hotmail.com>
2024-05-14 00:40:48 -0300

    redirect placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect } from "react";
+
 function Root() {
-  return <div>test</div>;
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "https://apexrocketry.wixsite.com/apex-rocketry/";
+    }, 500);
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="place-items-center grid bg-zinc-950 w-screen h-screen">
+      <Image
+        src="/apex.svg"
+        alt="Apex Rocketry UFSC"
+        width={500}
+        height={200}
+      />
+    </div>
+  );
 }
 
 export default Root;
